148ae6042d3cbdd31448787cb6c629a71ccffd9b
Sn0wFox <ruben.pericas@laposte.net>
2017-03-13 08:41:28 +0100

    Fixed char literals

--- src/parser/char_literal_parse.h
+++ src/parser/char_literal_parse.h
@@ -1,5 +1,5 @@
-#ifndef _CHAR_LITTERAL_PARSE_
-#define _CHAR_LITTERAL_PARSE_
+#ifndef _CHAR_LITERAL_PARSE_
+#define _CHAR_LITERAL_PARSE_
 
 int string_char_hex_to_int (char * str);
 int string_char_oct_to_int (char * str);
