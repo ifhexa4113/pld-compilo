d1251ba0066b26a1a70387de18051bec06b9961c
Sn0wFox <ruben.pericas@laposte.net>
2017-03-24 11:18:41 +0100

    Added test case for SymbolTable

---
+++ lib/parser/test/ast/symbol_table/SymbolTable.spec.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include "catch.h"
+#include "ast/symbol_table/SymbolTable.h"
+#include "ast/symbol_table/Variable.h"
+#include "ast/symbol_table/Type.h"
+
+TEST_CASE("It should state that the table contains an element", "SymbolTable")
+{
+    std::string id = "a";
+    Variable a(id, Type::CHAR_T);
+    SymbolTable st;
+
+    SECTION("It should insert a Symbol") {
+        st.put(id, &a);
+        REQUIRE(st.contains(id));
+
+        SECTION("It should gather the Symbol") {
+            REQUIRE(st.get(id) == &a);
+        }
+    }
+
+
+}
