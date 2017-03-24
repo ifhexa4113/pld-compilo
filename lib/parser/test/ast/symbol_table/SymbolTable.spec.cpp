1acb79b6d2d383dbe643f0cfe7a3e0cabe6ef9bd
Sn0wFox <ruben.pericas@laposte.net>
2017-03-24 12:20:20 +0100

    Trying to fix tests with Travis

--- lib/parser/test/ast/symbol_table/SymbolTable.spec.cpp
+++ lib/parser/test/ast/symbol_table/SymbolTable.spec.cpp
@@ -9,15 +9,10 @@ TEST_CASE("It should state that the table contains an element", "SymbolTable")
     std::string id = "a";
     Variable* a = new Variable(id, Type::CHAR_T);
     SymbolTable st;
+    st.put(id, a);
 
-    SECTION("It should insert a Symbol") {
-        st.put(id, a);
-        REQUIRE(st.contains(id));
-
-        SECTION("It should gather the Symbol") {
-            REQUIRE(st.get(id) == a);
-        }
-    }
+    REQUIRE(st.contains(id));
+    REQUIRE(st.get(id) == a);
 
     delete a;
 }
