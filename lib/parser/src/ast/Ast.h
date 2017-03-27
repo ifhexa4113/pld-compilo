79c11d827702f1ec165b6ad4a253c01042a19fca
Sn0wFox <ruben.pericas@laposte.net>
2017-03-27 18:33:10 +0200

    Removed AstIterator since it was not useful anymore

--- lib/parser/src/ast/Ast.h
+++ lib/parser/src/ast/Ast.h
@@ -1,17 +1,15 @@
 #ifndef AST_H
 #define AST_H
 
-#include "AstIterator.h"
 #include "block/CmmProgram.h"
 
 class Ast
 {
 public:
     CmmProgram& getProgram() { return root; };
-    AstIterator iterator() const { return AstIterator(this); };
 
 protected:
     CmmProgram root;
 };
 
-#endif
+#endif //AST_H
