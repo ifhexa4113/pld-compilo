b7dcf0f1268f409fe8befb8585b27eb97207edc2
Sn0wFox <ruben.pericas@laposte.net>
2017-03-28 10:54:03 +0200

    Added FunctionDefinitionTranslator

---
+++ src/cfg/translator/FunctionDefinitionTranslator.cpp
@@ -0,0 +1,59 @@
+#include "FunctionDefinitionTranslator.h"
+#include <iostream>
+
+FunctionDefinitionTranslator::FunctionDefinitionTranslator(FunctionDefinition* functionDef, CFG* cfg) : Translator(functionDef, cfg)
+{
+    // Nothing else to do
+}
+
+FunctionDefinitionTranslator::~FunctionDefinitionTranslator()
+{
+    // Nothing else to do
+}
+
+SubGraph * FunctionDefinitionTranslator::translate()
+{
+    // TODO: what about the return ???
+    // TODO: handle merging blocks here or after completing the whole cfg ?
+
+    // First cast it in something we can manipulate as we want
+    FunctionDefinition* fDef = dynamic_cast<FunctionDefinition*>(node);
+    if(fDef == nullptr)
+    {
+        std::cerr << "FunctionDefinitionTranslator::translate() : ERROR - associated node is not a FunctionDefinition" << std::endl;
+        return nullptr;
+    }
+
+    // Then create bases for the subgraph that we'll return
+    BasicBlock* functionBlock = new BasicBlock(fDef->getName());    // The function block; input of the subgraph
+    std::vector<BasicBlock*> outputs;                               // The list of subgraph's outputs
+
+    // Then create a variable to memorize the previous block
+    std::vector<BasicBlock*> previousBlocks(1, functionBlock);
+
+    // TODO: I think that in all cases, the first block could be merged in the functionBlock
+    // TODO     (replace label of the first block by the one of the functionBlock)
+
+    // For each child, link subgraphs
+    for(AstNode* child: fDef->getChildren())
+    {
+        Translator * t = getFactory().getTranslator(child, cfg);
+        SubGraph* sb = t->translate();
+        BasicBlock* bb = sb->getInput();
+
+        for(BasicBlock* output: previousBlocks)
+        {
+            // NOTE: if we're at the first child, this should never be executed
+            output->setExitTrue(bb);
+        }
+        previousBlocks = sb->getOutputs();
+        delete sb;
+        delete t;
+    }
+
+    outputs = previousBlocks;
+    // TODO: if there was a return; instruction somewhere else in the rest, we must add it to the outputs
+
+    // Return a subgraph describing what we just created
+    return new SubGraph(functionBlock, outputs);
+}
---
+++ src/cfg/translator/FunctionDefinitionTranslator.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_DEFINITION_TRANSLATOR_H
+#define FUNCTION_DEFINITION_TRANSLATOR_H
+
+#include "Translator.h"
+#include "ast/definition/FunctionDefinition.h"
+
+class FunctionDefinitionTranslator : public Translator
+{
+public:
+    FunctionDefinitionTranslator(FunctionDefinition* functionDef, CFG* cfg);
+    ~FunctionDefinitionTranslator();
+    SubGraph* translate();
+};
+
+
+#endif //FUNCTION_DEFINITION_TRANSLATOR_H
