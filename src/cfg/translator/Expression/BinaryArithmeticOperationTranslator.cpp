77fae020c2758d17cfa870595614e4d87205afc4
Charles <charles.belzile@insa-lyon.fr>
2017-03-29 14:04:30 -0300

    Fix some bugs in BinaryArithmeticOperationTranslator

--- src/cfg/translator/Expression/BinaryArithmeticOperationTranslator.cpp
+++ src/cfg/translator/Expression/BinaryArithmeticOperationTranslator.cpp
@@ -1,7 +1,13 @@
 #include "BinaryArithmeticOperationTranslator.h"
+#include "cfg/ir/AddInstruction.h"
+#include "cfg/ir/SubInstruction.h"
+#include "cfg/ir/MulInstruction.h"
+#include "cfg/ir/DivInstruction.h"
+#include "cfg/ir/ModInstruction.h"
+#include "cfg/ir/Register.h"
 #include <iostream>
 
-BinaryArithmeticOperationTranslator::BinaryArithmeticOperationTranslator(LiteralNumberExpression* literalNumberExpression, CFG* cfg) : Translator(literalNumberExpression, cfg)
+BinaryArithmeticOperationTranslator::BinaryArithmeticOperationTranslator(BinaryArithmeticOperation* binaryArithmeticOperation, CFG* cfg) : Translator(binaryArithmeticOperation, cfg)
 {
     // Nothing else to do
 }
@@ -26,38 +32,42 @@ SubGraph * BinaryArithmeticOperationTranslator::translate()
     std::vector<BasicBlock*> outputs(1, bb);
 
     Translator* leftT = getFactory().getTranslator( binArithOp->getLExpression, cfg);
-    Subgraph* leftSb = t->translate();
+    Subgraph* leftSb = leftT->translate();
     Translator* rightT = getFactory().getTranslator( binArithOp->getLExpression, cfg);
-    Subgraph* rightSb = t->translate();
+    Subgraph* rightSb = rightT->translate();
 
-    switch (binArithOp->getOperator){
-        case PLUS:
+    switch (binArithOp->getOperator()){
+        case ArithmeticOperator::PLUS:
             bb->addInstruction(new AddInstruction(new Register(),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(leftSb->getOutputs().back()->getInstructions().back())->getDestionation)),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(rightSb->getOutputs().back()->getInstructions().back())->getDestionation))));
             break;
-        case MINUS:
+        case ArithmeticOperator::MINUS:
             bb->addInstruction(new SubInstruction(new Register(),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(leftSb->getOutputs().back()->getInstructions().back())->getDestionation)),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(rightSb->getOutputs().back()->getInstructions().back())->getDestionation))));
             break;
-        case MUL:
+        case ArithmeticOperator::MUL:
             bb->addInstruction(new MulInstruction(new Register(),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(leftSb->getOutputs().back()->getInstructions().back())->getDestionation)),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(rightSb->getOutputs().back()->getInstructions().back())->getDestionation))));
             break;
-        case DIV:
+        case ArithmeticOperator::DIV:
             bb->addInstruction(new DivInstruction(new Register(),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(leftSb->getOutputs().back()->getInstructions().back())->getDestionation)),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(rightSb->getOutputs().back()->getInstructions().back())->getDestionation))));
             break;
-        case MOD:
+        case ArithmeticOperator::MOD:
             bb->addInstruction(new ModInstruction(new Register(),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(leftSb->getOutputs().back()->getInstructions().back())->getDestionation)),
                                                   new Register(*(dynamic_cast<RegisterInstruction*>(rightSb->getOutputs().back()->getInstructions().back())->getDestionation))));
             break;
     }
 
+    delete leftT;
+    delete leftSb;
+    delete rightT;
+    delete rightSb;
 
     // Return a subgraph describing what we just created
     return new SubGraph(bb, outputs);
