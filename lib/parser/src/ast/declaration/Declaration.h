18bdf226f9f1c802cb7502885dfd1b95f2c33016
msouka <soukaina.majdi@insa-lyon.fr>
2017-03-30 10:48:37 +0200

    Clone

--- lib/parser/src/ast/declaration/Declaration.h
+++ lib/parser/src/ast/declaration/Declaration.h
@@ -6,25 +6,17 @@
 #include "ast/AstNode.h"
 #include "ast/definition/Definition.h"
 #include <memory>
-class Clonable {
-    virtual ~clonable() {}
-    virtual clonable* clone() const = 0;
-};
-class Declaration: public AstNode, public Clonable
+class Declaration: public AstNode
 {
 public:
     Declaration(std::string name, Type type, int address = 0);
     virtual ~Declaration();
-    virtual Declaration* clone() const
-    { return new Declaration( *this ); }
     virtual int walkTree() = 0;
     virtual Definition* toEmptyDefinition() = 0;
     std::string getName() const;
     Type getType() const;
     int getAddress() const;
-
-private:
-    virtual Declaration* doClone() { return new Declaration(*this); }
+    virtual const shared_ptr<Declaration> clone() const =0;
 protected:
     std::string name;
     Type type;
--- lib/parser/src/ast/declaration/LValueDeclaration.h
+++ lib/parser/src/ast/declaration/LValueDeclaration.h
@@ -11,8 +11,10 @@ class LValueDeclaration : public Declaration
 public:
     LValueDeclaration(std::string name, Type type);
     virtual ~LValueDeclaration();
-    virtual LValueDeclaration* clone() const
-    { return new LValueDeclaration( *this ); }
+    virtual const shared_ptr<Declaration> clone() const
+    {
+        return shared_ptr<Declaration>( new LValueDeclaration(*this));
+    }
     virtual int walkTree() = 0;
     virtual Definition* toEmptyDefinition() = 0;
 };
