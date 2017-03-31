7d59b2a02e2a702915d96e026704eea24594425e
Adrien Moll <adrien.moll@insa-lyon.fr>
2017-03-31 12:46:44 +0200

    Fix bug return instruction translator

--- src/cfg/translator/instruction/ReturnInstructionTranslator.cpp
+++ src/cfg/translator/instruction/ReturnInstructionTranslator.cpp
@@ -24,31 +24,19 @@ SubGraph * ReturnInstructionTranslator::translate()
 
     // Then create bases for the subgraph that we'll return
     BasicBlock* retBlock = new BasicBlock();
-    std::vector<BasicBlock*> outputs;
 
-    // Then create a variable to memorize the previous block
-    std::vector<BasicBlock*> previousBlocks(1, retBlock);
+    Translator* t = getFactory().getTranslator(retInstr->getValue(), cfg);
+    SubGraph* sb = t->translate();
 
-    if(Translator * t = getFactory().getTranslator(retInstr->getValue(), cfg))
-    {
-        SubGraph* sb = t->translate();
-        BasicBlock* bb = sb->getInput();
-
-        for(BasicBlock* output: previousBlocks)
-        {
-            // NOTE: if we're at the first child, this should never be executed
-            output->setExitTrue(bb);
-        }
-        previousBlocks = sb->getOutputs();
-        delete sb;
-        delete t;
-    }
-
-    outputs = previousBlocks;
+    BasicBlock* expr = sb->getInput();
+    expr->setExitTrue(retBlock);
 
     //TODO: ABI (where should we store Expr's intermediates results and ret value) + MovInstr with 2 registers + No value for register ??????
-    retBlock->addInstruction(new MovInstruction(new Register(), new Register()));
+    retBlock->addInstruction(new MovInstruction(new Register(), new Register(*(dynamic_cast<RegisterInstruction*>(expr->getInstructions().back())->getDestination()))));
+
+    delete sb;
+    delete t;
 
     // Return a subgraph describing what we just created
-    return new SubGraph(retBlock, outputs);
+    return new SubGraph(expr, std::vector<BasicBlock*>());
 }
