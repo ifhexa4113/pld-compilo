4a5b0abeda6975acaf1f451e28d4415edd1302ec
Sn0wFox <ruben.pericas@laposte.net>
2017-04-05 08:40:43 +0200

    Table is now friend of LiteralNumber operand

--- src/cfg/ir/operand/LiteralNumber.h
+++ src/cfg/ir/operand/LiteralNumber.h
@@ -3,12 +3,40 @@
 
 #include "Operand.h"
 
-class LiteralNumber: public Operand {
+class Table;
+
+class LiteralNumber: public Operand
+{
+
+    /**
+     * The Table acts like an Operands' Priestess:
+     * She's the only one able to create Registers
+     * and manipulate them.
+     */
+    friend class Table;
+
 public:
-    LiteralNumber(int value_);
+    /**
+     * This is a the correct way of doing an operator= overload and so:
+     * using the copy-and-swap idiom.
+     * http://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
+     */
+    friend void swap(LiteralNumber& ln1, LiteralNumber& ln2);
+
+    /**
+     * Transforms the operand into a string,
+     * in order to insert it in a stream for example.
+     */
+    std::string toString() const;
+
+    // Classic destructor
     ~LiteralNumber();
 
-    std::string toString() const;
+private:
+    LiteralNumber(int value_ = 0);                  // Classic
+    LiteralNumber(LiteralNumber const & other);     // Copy
+    LiteralNumber(LiteralNumber&& other);           // Move
+    LiteralNumber& operator=(LiteralNumber other);  // With copy-and-swap idiom, so no const ref
 
 };
 
