a11d255fc332b8d184cd9d8419acf047af48e919
Axel Lefrant <axel.lefrant@insa-lyon.fr>
2017-04-05 19:17:42 +0200

    The While can now contain graph in its body

--- src/cfg/translator/block/WhileTranslator.cpp
+++ src/cfg/translator/block/WhileTranslator.cpp
@@ -43,32 +43,67 @@ SubGraph * WhileTranslator::translate(Table* table)
     delete ct;
 
     // Then create the block where to body will lie
-    BasicBlock* body = new BasicBlock();
+    BasicBlock* body = new BasicBlock("");
     // And be sure to link the condition's exitFalse to it
-    conditionBlockOutput->setExitFalse(body);
 
-    // Then create a variable to memorize the previous block
-    // std::vector<BasicBlock*> previousBlocks;
 
-    // Then gather the subgraph from children
-    for(AstNode* child: wh->getChildren())
+    std::vector<AstNode*> children = wh->getChildren();
+    if(children.empty())
     {
-        if(Translator * t = getFactory().getTranslator(child, cfg))
+        conditionBlockOutput->setExitFalse(body);
+        body->setExitTrue(conditionBlockInput);
+    }
+
+    std::vector<BasicBlock*> previousOutputs;
+    if(Translator * t = getFactory().getTranslator(children.front(), cfg))
+    {
+        SubGraph* sb = t->translate(table);
+        body = sb->getInput();
+        if(body->getLabel() == "")
+        {
+            body->giveLabel();
+        }
+        conditionBlockOutput->setExitFalse(body);
+
+        std::cout << body->getLabel() << std::endl;
+        for(auto& instr : body->getInstructions())
+        {
+            instr->print(std::cout);
+        }
+
+        previousOutputs = sb->getOutputs();
+        delete sb;
+        delete t;
+    }
+
+    for(int i = 1; i < children.size(); i++)
+    {
+        if(Translator * t = getFactory().getTranslator(children[i], cfg))
         {
             SubGraph* sb = t->translate(table);
             BasicBlock* bb = sb->getInput();
+            if(bb->getLabel() == "")
+            {
+                // TODO merge instead of giving a label ?
+                bb->giveLabel();
+            }
+            for(BasicBlock* currentOutput : previousOutputs)
+            {
+                currentOutput->setExitTrue(bb);
+            }
 
-            // Just merge it for the moment
-            body->merge(bb);
-
-            // TODO: find something better than a merge
-
+            previousOutputs = sb->getOutputs();
             delete sb;
             delete t;
         }
     }
 
-    body->setExitTrue(conditionBlockInput);
+
+    // end of the loop, go back to the test condition
+    for(BasicBlock* finalOutput : previousOutputs)
+    {
+        finalOutput->setExitTrue(conditionBlockInput);
+    }
 
     // Eventually return a subgraph describing what we just created
     return new SubGraph(conditionBlockInput, std::vector<BasicBlock*>(1, conditionBlockOutput));
